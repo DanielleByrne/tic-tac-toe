bf9398bb5fed3fd21d48fc7922f429c2e2c93592
Danielle Byrne <DanielleByrne421@gmail.com>
2022-01-29 22:35:38 -0800

    the index and current player are populating in each button but it's happening all at once instead of on a click

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-const cells = document.querySelectorAll("button.cell")
- console.log(cells)
 
-cells.forEach(cell => {
-    cell.addEventListener('click', handleClick)
+// grabbing each button with the class of cell and saving it to a variable
+const cells = document.querySelectorAll("button.cell")
+
+// grabbing the player turn area 
+const playerTurn = document.querySelector(".player-turn")
+
+// grabbing the reset button
+const resetButton = document.querySelector(".reset-button")
+
+
+
+//setting up an empty array for the game, a current player, and that the game is running
+let gameBoard = ['','','','','','','','','',]
+let currentPlayer= "O"
+let gameActive= true
+
+//looping over each cell and adding an event listener to it
+// {once: true} makes it so that it can only be clicked once 
+
+const handleClick = (cell, index) => {
+    if (gameActive === true){
+        cell.innerHTML= currentPlayer
+    } else (console.log("game isn't active"))
+    console.log(currentPlayer, index)
+}
+
+
+// adding an event listener to each button with cell class
+cells.forEach((cell, index ) => {
+    cell.addEventListener("click", handleClick(cell, index), {once: true})
 })
 
-function handleClick(event){
-    console.log("clicked")
-}
+
+
+
+const playerXWins = "Player X Won"
+const playerOWins = "Player O Won"
+const tie = "Tie"
+
+
